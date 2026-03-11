91bfbaa97227f3c30b822c546f298e8520d2333c
Arun Thangavel <aru701567@gmail.com>
2026-03-11 13:35:48 +0530

    Let the preflight request from client side

--- src/main/java/com/example/EDA/Routing/FunctionalRouting.java
+++ src/main/java/com/example/EDA/Routing/FunctionalRouting.java
@@ -4,12 +4,14 @@ import com.example.EDA.RequestHandler.PollRequestHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class FunctionalRouting {
     private final PollRequestHandler pollRequestHandler;
     @Bean
